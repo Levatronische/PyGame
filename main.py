5be5bc279fedc341072bdfb4d41c241e39bb7e17
Levatronische <95122626+Levatronische@users.noreply.github.com>
2022-01-29 13:46:51 +0300

    Add QT and Time

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 import math
 import random
-
-WIDTH, HEIGHT = 1600, 900
-
-sky_box_long = 500
-
-sky_box = [open("sky_box/back.png"), open("sky_box/bottom.png"),
-           open("sky_box/front.png"), open("sky_box/left.png"),
-           open("sky_box/right.png"), open("sky_box/top.png"), ]
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+import sys
+import datetime
 
 first_map = [
-    [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
+    [(5, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
     [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
     [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
     [(0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0), (0, 0)],
@@ -46,6 +43,20 @@ test_map = [
     [(0, 0), (0, 0), (0, 0)],
     [(5, 270), (0, 0), (5, 180)]
 ]
+
+loc_m = [["big_map", first_map], ["small_map", small_map], ["test_map", test_map]]
+
+game_loc = None
+
+screen_resolution = [
+    ["5120x2880", 5120, 2880], ["3840x2160", 3840, 2160], ["2560x1440", 2560, 1440],
+    ["1920x1080", 1920, 1080], ["1600x900", 1600, 900], ["1280x1024", 1280, 1024]
+                     ]
+
+name = ""
+
+
+sky_box_long = 500
 
 cam_loc = [0, 0]
 cam_ang_xy = [0, 0]
@@ -297,7 +308,6 @@ def check_v_coll(cam_loc_x, cam_loc_y):
     for i in v_coll_list:
         if (i[0] + 0.2 >= cam_loc_x >= i[0] - 0.2) \
                 and (i[1] + 1 >= cam_loc_y >= i[1] - 1):
-                print("coll v")
                 return True
     return False
 
@@ -530,190 +540,214 @@ def draw_map(g_map):
         y_pos -= 1
 
 
-pygame.init()
-display = (WIDTH, HEIGHT)
-scree = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+class MainStartWnd(QWidget):
+    def __init__(self):
+        super().__init__()
 
-glEnable(GL_DEPTH_TEST)
-glEnable(GL_LIGHTING)
-glShadeModel(GL_SMOOTH)
-glEnable(GL_COLOR_MATERIAL)
-glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+        self.ui = uic.loadUi('qtStartWindow.ui', self)
+        self.ui.setWindowIcon(QIcon(""))
 
-glEnable(GL_LIGHT0)
-glLightfv(GL_LIGHT0, GL_AMBIENT, [0.5, 0.5, 0.55, 1])
-glLightfv(GL_LIGHT0, GL_DIFFUSE, [0.6, 0.5, 0.3, 1])
+        self.comboBoxScreenResolution.addItems([i[0] for i in screen_resolution])
+        self.comboBoxLoc.addItems([i[0] for i in loc_m])
+        self.pushButtonStart.clicked.connect(self.startGame)
 
-sphere = gluNewQuadric()
+        self.show()
 
-glMatrixMode(GL_PROJECTION)
-gluPerspective(75, (display[0] / display[1]), 0.01, 5000)
+    def startGame(self):
+        WIDTH, HEIGHT = screen_resolution[self.comboBoxScreenResolution.currentIndex()][1],\
+                        screen_resolution[self.comboBoxScreenResolution.currentIndex()][2]
+        game_loc = loc_m[self.comboBoxLoc.currentIndex()][1]
+        name = str(self.lineEditName.text())
 
-glMatrixMode(GL_MODELVIEW)
-gluLookAt(0, -2, 0,
-          0, 0, 0,
-          0, 0, 1)
-viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
-glLoadIdentity()
+        prepare_map(game_loc)
 
-displayCenter = [scree.get_size()[i] // 2 for i in range(2)]
-mouseMove = [0, 0]
-pygame.mouse.set_pos(displayCenter)
-pygame.mouse.set_visible(False)
-up_down_angle = 0
-left_right_angle_world = 0
-left_right_angle = 0
-paused = False
-run = True
-in_start = True
+        pygame.init()
+        display = (WIDTH, HEIGHT)
+        scree = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-loadTexture()
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_LIGHTING)
+        glShadeModel(GL_SMOOTH)
+        glEnable(GL_COLOR_MATERIAL)
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
 
-have_key = False
+        glEnable(GL_LIGHT0)
+        glLightfv(GL_LIGHT0, GL_AMBIENT, [0.5, 0.5, 0.55, 1])
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, [0.6, 0.5, 0.3, 1])
 
-prepare_map(small_map)
+        sphere = gluNewQuadric()
 
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            pygame.mouse.set_visible(True)
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE or event.key == pygame.K_RETURN:
-                run = False
-            if event.key == pygame.K_PAUSE or event.key == pygame.K_p:
-                paused = not paused
-                pygame.mouse.set_pos(displayCenter)
-        if not paused:
-            if event.type == pygame.MOUSEMOTION:
-                mouseMove = [event.pos[i] - displayCenter[i] for i in range(2)]
-            pygame.mouse.set_pos(displayCenter)
+        glMatrixMode(GL_PROJECTION)
+        gluPerspective(75, (display[0] / display[1]), 0.01, 5000)
 
-        if paused:
-            pygame.mouse.set_visible(True)
-            do_timer(False)
-
-    if not paused:
-
-        if (key_loc[0] + 1.5 >= cam_loc[0] >= key_loc[0] - 1.5) \
-                and (key_loc[1] + 1.5 >= cam_loc[1] >= key_loc[1] - 1.5):
-            #            print("Key")
-            have_key = True
-
-        keypress = pygame.key.get_pressed()
-        pygame.mouse.set_visible(False)
-        do_timer(True)
-        glLoadIdentity()
-
-        up_down_angle += mouseMove[1] * 0.1
-        glRotatef(up_down_angle, 1.0, 0.0, 0.0)
-
-        glPushMatrix()
-
-        glLoadIdentity()
-
-        if in_start:
-            glTranslatef(0, 0, 2)
-            in_start = False
-
-        d_x = 0
-        d_y = 0
-
-        forward_backward = 0
-        right_left = 0
-
-        if keypress[pygame.K_w] or keypress[pygame.K_UP]:
-            #            glTranslatef(0, 0, 0.1)
-            d_x += math.sin(math.radians(left_right_angle)) * x_speed
-            d_y += math.cos(math.radians(left_right_angle)) * y_speed
-            forward_backward += 0.1
-        if keypress[pygame.K_s] or keypress[pygame.K_DOWN]:
-            #            glTranslatef(0, 0, -0.1)
-            d_x += math.sin(math.radians(left_right_angle)) * -x_speed
-            d_y += math.cos(math.radians(left_right_angle)) * -y_speed
-            forward_backward += -0.1
-        if keypress[pygame.K_d] or keypress[pygame.K_LEFT]:
-            #            glTranslatef(-0.1, 0, 0)
-            d_x += math.sin(math.radians(90 - left_right_angle)) * x_speed
-            d_y += math.cos(math.radians(90 - left_right_angle)) * -y_speed
-            right_left += -0.1
-        if keypress[pygame.K_a] or keypress[pygame.K_RIGHT]:
-            #            glTranslatef(0.1, 0, 0)
-            d_x += math.sin(math.radians(90 - left_right_angle)) * -x_speed
-            d_y += math.cos(math.radians(90 - left_right_angle)) * y_speed
-            right_left += 0.1
-
-        if check_h_coll(cam_loc[0] + d_x, cam_loc[1] + d_y):
-            d_y = 0
-            d_x = 0
-            if right_left != 0:
-                right_left = 0
-            if forward_backward != 0:
-                forward_backward = 0
-
-        if check_v_coll(cam_loc[0] + d_x, cam_loc[1] + d_y):
-            d_y = 0
-            d_x = 0
-            if right_left != 0:
-                right_left = 0
-            if forward_backward != 0:
-                forward_backward = 0
-
-    #    print(forward_backward, right_left)
-
-        glTranslatef(right_left, 0, forward_backward)
-
-        cam_loc[0] += d_x
-        cam_loc[1] += d_y
-
-        left_right_angle_world += mouseMove[0] * 0.1
-        left_right_angle = left_right_angle_world - (left_right_angle_world // 360) * 360
-    #    print(f"ang:{left_right_angle}, x:{cam_loc[0]}, y:{cam_loc[1]}")
-
-        glRotatef(mouseMove[0] * 0.1, 0.0, 1.0, 0.0)
-
-        glMultMatrixf(viewMatrix)
+        glMatrixMode(GL_MODELVIEW)
+        gluLookAt(0, -2, 0,
+                  0, 0, 0,
+                  0, 0, 1)
         viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+        glLoadIdentity()
 
-        glPopMatrix()
+        displayCenter = [scree.get_size()[i] // 2 for i in range(2)]
+        mouse_move = [0, 0]
+        pygame.mouse.set_pos(displayCenter)
+        pygame.mouse.set_visible(False)
+        up_down_angle = 0
+        left_right_angle_world = 0
+        left_right_angle = 0
+        loadTexture()
+        paused = False
+        run = True
+        in_start = True
+        have_key = False
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.mouse.set_visible(True)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE or event.key == pygame.K_RETURN:
+                        run = False
+                    if event.key == pygame.K_PAUSE or event.key == pygame.K_p:
+                        paused = not paused
+                        pygame.mouse.set_pos(displayCenter)
+                if not paused:
+                    if event.type == pygame.MOUSEMOTION:
+                        mouse_move = [event.pos[i] - displayCenter[i] for i in range(2)]
+                    pygame.mouse.set_pos(displayCenter)
 
-        glMultMatrixf(viewMatrix)
+                if paused:
+                    pygame.mouse.set_visible(True)
+                    do_timer(False)
 
-        glLightfv(GL_LIGHT0, GL_POSITION, [1, -1, 1, 0])
+            if not paused:
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        #
-        #
-        #
-        draw_map(small_map)
-        #
-        #
-        #
-        glPushMatrix()
+                if (key_loc[0] + 1.5 >= cam_loc[0] >= key_loc[0] - 1.5) \
+                        and (key_loc[1] + 1.5 >= cam_loc[1] >= key_loc[1] - 1.5):
+                    #            print("Key")
+                    have_key = True
 
-        build_sky_box()
+                keypress = pygame.key.get_pressed()
+                pygame.mouse.set_visible(False)
+                do_timer(True)
+                glLoadIdentity()
 
-        glColor4f(1, 0.843, 0, 1)
+                up_down_angle += mouse_move[1] * 0.1
+                glRotatef(up_down_angle, 1.0, 0.0, 0.0)
 
-        glTranslatef(key_loc[0], key_loc[1], 0)
+                glPushMatrix()
 
-        if not have_key:
-            gluSphere(sphere, 1, 32, 64)
+                glLoadIdentity()
 
-        glPopMatrix()
-        #        print(cam_loc)
-        glPushMatrix()
-        glColor4f(0.545, 0, 1, 1)
-        if have_key:
-            glColor4f(0.545, 0, 1, 1)
-            gluSphere(sphere, 1, 32, 64)
-            if (0 + 1.5 >= cam_loc[0] >= 0 - 1.5) \
-                    and (0 + 1.5 >= cam_loc[1] >= 0 - 1.5):
-                print(f"you win!!! time: {do_timer(False)}")
-                run = False
-        glPopMatrix()
+                if in_start:
+                    glTranslatef(0, 0, 2)
+                    in_start = False
 
-        pygame.display.flip()
-        pygame.time.wait(10)
+                dx = 0
+                dy = 0
+                forward_backward = 0
+                right_left = 0
 
-pygame.quit()
+                if keypress[pygame.K_w] or keypress[pygame.K_UP]:
+                    #            glTranslatef(0, 0, 0.1)
+                    dx += math.sin(math.radians(left_right_angle)) * x_speed
+                    dy += math.cos(math.radians(left_right_angle)) * y_speed
+                    forward_backward += 0.1
+                if keypress[pygame.K_s] or keypress[pygame.K_DOWN]:
+                    #            glTranslatef(0, 0, -0.1)
+                    dx += math.sin(math.radians(left_right_angle)) * -x_speed
+                    dy += math.cos(math.radians(left_right_angle)) * -y_speed
+                    forward_backward += -0.1
+                if keypress[pygame.K_d] or keypress[pygame.K_LEFT]:
+                    #            glTranslatef(-0.1, 0, 0)
+                    dx += math.sin(math.radians(90 - left_right_angle)) * x_speed
+                    dy += math.cos(math.radians(90 - left_right_angle)) * -y_speed
+                    right_left += -0.1
+                if keypress[pygame.K_a] or keypress[pygame.K_RIGHT]:
+                    #            glTranslatef(0.1, 0, 0)
+                    dx += math.sin(math.radians(90 - left_right_angle)) * -x_speed
+                    dy += math.cos(math.radians(90 - left_right_angle)) * y_speed
+                    right_left += 0.1
+
+                if check_h_coll(cam_loc[0] + dx, cam_loc[1] + dy):
+                    dy = 0
+                    dx = 0
+                    if right_left != 0:
+                        right_left = 0
+                    if forward_backward != 0:
+                        forward_backward = 0
+
+                if check_v_coll(cam_loc[0] + dx, cam_loc[1] + dy):
+                    dy = 0
+                    dx = 0
+                    if right_left != 0:
+                        right_left = 0
+                    if forward_backward != 0:
+                        forward_backward = 0
+
+            #    print(forward_backward, right_left)
+
+                glTranslatef(right_left, 0, forward_backward)
+
+                cam_loc[0] += dx
+                cam_loc[1] += dy
+
+                left_right_angle_world += mouse_move[0] * 0.1
+                left_right_angle = left_right_angle_world - (left_right_angle_world // 360) * 360
+            #    print(f"ang:{left_right_angle}, x:{cam_loc[0]}, y:{cam_loc[1]}")
+
+                glRotatef(mouse_move[0] * 0.1, 0.0, 1.0, 0.0)
+
+                glMultMatrixf(viewMatrix)
+                viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
+
+                glPopMatrix()
+
+                glMultMatrixf(viewMatrix)
+
+                glLightfv(GL_LIGHT0, GL_POSITION, [1, -1, 1, 0])
+
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+                #
+                #
+                #
+                draw_map(game_loc)
+                #
+                #
+                #
+                glPushMatrix()
+
+                build_sky_box()
+
+                glColor4f(1, 0.843, 0, 1)
+
+                glTranslatef(key_loc[0], key_loc[1], 0)
+
+                if not have_key:
+                    gluSphere(sphere, 1, 32, 64)
+
+                glPopMatrix()
+                #        print(cam_loc)
+                glPushMatrix()
+                glColor4f(0.545, 0, 1, 1)
+                if have_key:
+                    glColor4f(0.545, 0, 1, 1)
+                    gluSphere(sphere, 1, 32, 64)
+                    if (0 + 1.5 >= cam_loc[0] >= 0 - 1.5) \
+                            and (0 + 1.5 >= cam_loc[1] >= 0 - 1.5):
+                        dt_now = datetime.datetime.now()
+                        self.labelTime.setText(f"{do_timer(False)}")
+                        results = open("results.txt", mode="w")
+                        results.write(f"{dt_now}: {name} - {do_timer(False)}")
+                        results.close()
+                        run = False
+                glPopMatrix()
+
+                pygame.display.flip()
+                pygame.time.wait(10)
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainStartWnd()
+    sys.exit(app.exec_())
